47a23f2970a05e95844de5a07a6f8e5452996fb2
thivux <hanchungluong@gmail.com>
2024-11-24 18:38:44 +0700

    resample vivoice

--- resample.py
+++ resample.py
@@ -1,3 +1,5 @@
+import random
+from datasets import load_dataset
 from tqdm import tqdm
 import pandas as pd
 import os
@@ -24,12 +26,33 @@ def process_files(source_paths, target_dir):
 
 if __name__ == "__main__":
     # sachnoi
-    rootdir = '/lustre/scratch/client/vinai/users/thivt1/code/oneshot'
-    data = pd.read_csv(os.path.join(rootdir, 'artifacts/step19_augmented_train.csv'), sep='|', names=['path', 'text', 'speaker', 'duration'])
-    data = data.sample(1000, random_state=42)
-    source_paths = data['path'].tolist()
-    source_paths = [os.path.join(rootdir, path) for path in source_paths]
+    # rootdir = '/lustre/scratch/client/vinai/users/thivt1/code/oneshot'
+    # data = pd.read_csv(os.path.join(rootdir, 'artifacts/step19_augmented_train.csv'), sep='|', names=['path', 'text', 'speaker', 'duration'])
+    # data = data.sample(1000, random_state=42)
+    # source_paths = data['path'].tolist()
+    # source_paths = [os.path.join(rootdir, path) for path in source_paths]
 
-    target_folder = "sachnoi-8khz"
+    # target_folder = "sachnoi-8khz"
+    # os.makedirs(target_folder, exist_ok=True)
+    # process_files(source_paths, target_folder)
+
+    # vivoice 
+    repo = "capleaf/viVoice"                                                                                                                  
+    dataset = load_dataset(repo, use_auth_token='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')                                                          
+    print(dataset)
+    dataset = dataset['train']
+    random.seed(42)
+
+    # Randomly sample 1000 samples
+    sampled_dataset = dataset.shuffle(seed=42).select(range(1000))
+    # save the sampled dataset to a folder 
+    target_folder = "vivoice-16khz"
     os.makedirs(target_folder, exist_ok=True)
-    process_files(source_paths, target_folder)
+    for sample in tqdm(sampled_dataset):
+        audio = sample['audio']
+        wav = audio['array']
+        sr = audio['sampling_rate']
+        path = sample['path']
+        target_path = os.path.join(target_folder, path)
+        librosa.save(audio, target_path, sr=16000)
+
