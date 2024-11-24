eb1e256d4ef3511687d71593d47ca89acdea7087
thivux <hanchungluong@gmail.com>
2024-11-25 01:06:35 +0700

    vlsp speaking rate

--- speaking_rate.py
+++ speaking_rate.py
@@ -103,7 +103,7 @@ def vinbigdata():
     data = pd.read_csv('metadata/vinbigdata.csv')
 
     wpms = []
-    for i, row in data.iterrows(): 
+    for i, row in tqdm(data.iterrows()): 
         wav_path, duration = row['path'], row['duration']
         txt_path = wav_path.replace('.wav', '.txt')
         with open(txt_path, 'r') as f:
@@ -118,11 +118,42 @@ def vinbigdata():
     print('done processing speaking rate for vinbigdata dataset')
 
     
+def vlsp(): 
+    # duration mapping 
+    data = pd.read_csv('metadata/vlsp.csv')
+    duration_map = {}
+    for i, row in data.iterrows():
+        duration_map[row['path']] = row['duration']
+
+    rootdir = '/home/thivt1/data/VLSP_data'
+    dev2021 = os.path.join(rootdir, 'VLSP-ASR-2021-labeled_devset')
+
+    # read the transcript file 
+    transcript_file = os.path.join(dev2021, 'transcript.txt')
+    dev2021_map = {}
+    with open(transcript_file, 'r') as f:
+        for line in f: 
+            name, transcript = line.strip().split('\t')
+            dev2021_map[name + ".wav"] = normalize_transcript(transcript)
+    
+    # list file in the wav folder
+    wav_files = get_wav_files(os.path.join(dev2021, 'wav'))
+    metadata1 = []
+    for file in tqdm(wav_files): 
+        breakpoint()
+        name = os.path.basename(file)
+        duration = duration_map[file]
+        transcript = dev2021_map[name]
+        wps = len(transcript.split()) / float(duration)
+        wpm = wps * 60
+        metadata1.append([name, transcript, duration, wpm])
+
+
 if __name__ == '__main__': 
     # sachnoi()
     # vin27()
     # vivoice()
     # bud500()
-    vinbigdata()
+    # vinbigdata()
     # vivoice()
-    # vlsp()
+    vlsp()
